acc37d5456acf8ad939f5bcc9df402222807059e
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-07-19 10:55:20 -0400

    commit tree ea32c6ed5c7bddc59a994cfe1c6b079a6f3c7d7b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+//Using the Array filter method
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers,string){
+    const name = drivers.filter (driversName => driversName.toLowerCase() === string.toLowerCase())
+    return name
+}
+function fuzzyMatch (drivers,string){
+    const name = drivers.filter (driversName.startWith(string))
+    return name
+}
+function matchName (drivers, string){
+    const name = drivers.filter (driversName => driversName.match)
+    return name
+}
+
+  
+  
+
+
+
+
+
+
